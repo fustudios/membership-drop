246a7b65a177e2b431f25b4634d7744c22f53270
Jiri Blaha <blaha@wedecom.cz>
2024-05-27 22:32:47 +0200

    Finished, imprecise

--- index.js
+++ index.js
@@ -1,28 +1,112 @@
 const fs = require("fs");
 
 const snapshots = [1, 2, 3];
+const graphUrl = 'https://api.thegraph.com/subgraphs/name/bowsernet/open-season-staging';
+const graphPageSize = 1000;
+const maxPage = 10;
+const multipliers = [1.2, 1.1, 1];
+const numFuTokens = 62_100_000;
 
-function main(){
-    const dataIn = snapshots.map(snapshot => readSnapshotData(snapshot));
-    const dataOut = snapshots.map(_ => ({}));
-
-    for (let [addr, amount3] of Object.entries(dataIn[2])) {
-        let amount1 = dataIn[0][addr] || 0;
-        let amount2 = dataIn[1][addr] || 0;
-
-        amount1 = Math.min(amount1, amount2, amount3);
-        amount2 = Math.min(amount2, amount3);
-        amount2 -= amount1;
-        amount3 -= (amount1 + amount2);
-
-        if (amount1 > 0) dataOut[0][addr] = amount1;
-        if (amount2 > 0) dataOut[1][addr] = amount2;
-        if (amount3 > 0) dataOut[2][addr] = amount3;
+const getGraphQuery = (afterId = 0) => `
+{
+    nfts(
+        first: ${graphPageSize},
+        orderBy: tokenId,
+        where:{ collection: "0x1c67d8f07d7ef2d637e61ed3fbc3fa9aaf7a6267", tokenId_gt: "${afterId}"}
+    ) {
+        tokenId
+        user { id }
     }
 }
+`;
 
-function readSnapshotData(snapshotNumber) {
-    const data = fs.readFileSync(`snapshot${snapshotNumber}.txt`, 'utf8');
+async function main(){
+    // writeSnapshotData('snapshot3.txt', await readSnapshot3(), '');
+
+    const balancesIn = snapshots.map(snapshot => readSnapshotData(`snapshot${snapshot}.txt`));
+    const balances = recalcSnapshots(balancesIn);
+    const weightedBalances = computeWeightedBalances(balances);
+    const fuBalances = computeFuDrops(weightedBalances);
+
+    snapshots.map(snapshot => writeSnapshotData(`snapshot${snapshot}Out.txt`, balances[snapshot - 1]));
+    writeSnapshotData('FU.csv', fuBalances);
+
+    console.log('FU sum', sumBalances(fuBalances));
+}
+
+function recalcSnapshots(balancesIn) {
+    const result = snapshots.map(_ => ({}));
+
+    for (let [addr, balance3] of Object.entries(balancesIn[2])) {
+        let balance1 = balancesIn[0][addr] || 0;
+        let balance2 = balancesIn[1][addr] || 0;
+
+        balance1 = Math.min(balance1, balance2, balance3);
+        balance2 = Math.min(balance2, balance3);
+        balance2 -= balance1;
+        balance3 -= (balance1 + balance2);
+
+        if (balance1 > 0) result[0][addr] = balance1;
+        if (balance2 > 0) result[1][addr] = balance2;
+        if (balance3 > 0) result[2][addr] = balance3;
+    }
+    return result;
+}
+
+function computeWeightedBalances(balances) {
+    let result = {};
+    for (let i = 0; i < 3; i++) {
+        for (const [addr, balance] of Object.entries(balances[i])) {
+            result[addr] = (result[addr] || 0) + balance * multipliers[i];
+        }
+    }
+    return result;
+}
+
+function computeFuDrops(weightedBalances) {
+    const weightedSum = sumBalances(weightedBalances);
+    const fuPerBalance = numFuTokens / weightedSum;
+    const result = {};
+    for (const [addr, balance] of Object.entries(weightedBalances)) {
+        result[addr] = balance * fuPerBalance;
+    }
+    return result;
+}
+
+function sumBalances(data) {
+    return Object.values(data).reduce((sum, count) => sum + parseFloat(count), 0);
+}
+
+async function readSnapshot3() {
+    let result = {};
+    let lastId = '0';
+    let page = 0;
+    do {
+       const holders = await fetchHoldersAfterId(lastId);
+       for (const { holder } of holders) {
+          result[holder] = (result[holder] || 0) + 1;
+       }
+       if (holders.length === 0) break;
+       page++;
+       lastId = holders[holders.length - 1].tokenId;
+    } while (page < maxPage);
+    return result;
+}
+
+async function fetchHoldersAfterId(id) {
+    const query = getGraphQuery(id);
+    const response = await fetch(graphUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ query })
+    });
+    const json = await response.json();
+    const { data } = json;
+    return data.nfts.map(item => ({ holder: item.user.id, tokenId: item.tokenId }));
+}
+
+function readSnapshotData(filename) {
+    const data = fs.readFileSync(filename, 'utf8');
     let result = {};
     for (const row of data.trim().split("\n")) {
         const [address, balance] = row.split(',').map(x => x.trim());
@@ -31,11 +115,11 @@ function readSnapshotData(snapshotNumber) {
     return result;
 }
 
-function writeSnapshotData(snapshotNumber, data) {
+function writeSnapshotData(filename, data) {
     fs.writeFileSync(
-        `snapshot${snapshotNumber}Out.txt`,
+        filename,
         Object.entries(data).map(([addr, balance]) => `${addr},${balance}`).join("\n")
     );
 }
 
-main();
+main().catch(e => console.error(e));
