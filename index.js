c5f83d0f171ea57eb6ca2e990bbec28df865001c
Jiri Blaha <blaha@wedecom.cz>
2024-05-27 18:21:10 +0200

    correct algo, csv input, same output, simplified the fuck out of it

--- index.js
+++ index.js
@@ -1,97 +1,41 @@
 const fs = require("fs");
 
+const snapshots = [1, 2, 3];
+
 function main(){
- 
-    var ssh1Address = new Array();
-    var ssh1NFTs = new Array();
-    var ssh2Address = new Array();
-    var ssh2NFTs = new Array();
-    var ssh3Address = new Array();
-    var ssh3NFTs = new Array();
+    const dataIn = snapshots.map(snapshot => readSnapshotData(snapshot));
+    const dataOut = snapshots.map(_ => ({}));
 
-    var ss1 = {};
-    var ss2 = {};
-    var ss3 = {};
+    for (let [addr, amount3] of Object.entries(dataIn[2])) {
+        let amount1 = dataIn[0][addr] || 0;
+        let amount2 = dataIn[1][addr] || 0;
 
-    function readFileAndSplit(path) {
-        const data = fs.readFileSync(path, 'utf8');
-        return data.toString().split(",");
+        amount1 = Math.min(amount1, amount2, amount3);
+        amount2 = Math.min(amount2, amount3);
+        amount2 -= amount1;
+        amount3 -= (amount1 + amount2);
+
+        if (amount1 > 0) dataOut[0][addr] = amount1;
+        if (amount2 > 0) dataOut[1][addr] = amount2;
+        if (amount3 > 0) dataOut[2][addr] = amount3;
     }
-
-    function readAndAssign() {
-        try {
-            ssh1Address = readFileAndSplit("ss1AddressList.txt");
-            ssh2Address = readFileAndSplit("ss2AddressList.txt");
-            ssh3Address = readFileAndSplit("ss3AddressList.txt");
-            ssh1NFTs = readFileAndSplit("ss1NFTList.txt");
-            ssh2NFTs = readFileAndSplit("ss2NFTList.txt");
-            ssh3NFTs = readFileAndSplit("ss3NFTList.txt");
-        } catch (err) {
-            console.error(err);
-        }
-    }
-    function compute()
-    {
-        for (let i in ssh3Address) 
-        {
-            for (let j in ssh2Address) 
-            {
-                if(ssh3Address[i] == ssh2Address[j])
-                {
-                    for (let k in ssh1Address) 
-                    { 
-                        if(ssh2Address[j] == ssh1Address[k])
-                        {
-                            ss1[ssh1Address[k]] = Math.min(ssh1NFTs[k],ssh2NFTs[j], ssh3NFTs[i]);
-                        }
-                    }
-                    var min = Math.min(ssh2NFTs[j], ssh3NFTs[i]);
-                    if(ss1[ssh2Address[j]] !== undefined) 
-                    {
-                        var total = min - ss1[ssh2Address[j]];
-                        if(total != 0)
-                        {
-                            ss2[ssh2Address[j]] = total;
-                        }
-                    }
-                }
-            }
-            
-            var NFTcache = ssh3NFTs[i]
-            
-            if(ss1[ssh3Address[i]] !== undefined)
-            {
-                NFTcache -= ss1[ssh3Address[i]] 
-            } 
-            
-            if(ss2[ssh3Address[i]] !== undefined)
-            {
-                NFTcache -= ss2[ssh3Address[i]]
-            }
-            
-            if(NFTcache != 0)
-            {
-                ss3[ssh3Address[i]]  = NFTcache
-            }
-        }
-
-        fs.writeFileSync('./ss1Final.txt', objToString(ss1));
-        fs.writeFileSync('./ss2Final.txt', objToString(ss2));
-        fs.writeFileSync('./ss3Final.txt', objToString(ss3));
-    }
-
-    function objToString (obj) {
-        let str = '';
-        for (const [p, val] of Object.entries(obj)) {
-            str += `${p},${val}\n`;
-        }
-        return str;
-    }
-
-    readAndAssign();
-    compute();
 }
 
+function readSnapshotData(snapshotNumber) {
+    const data = fs.readFileSync(`snapshot${snapshotNumber}.txt`, 'utf8');
+    let result = {};
+    for (const row of data.trim().split("\n")) {
+        const [address, balance] = row.split(',').map(x => x.trim());
+        result[address] = balance;
+    }
+    return result;
+}
 
+function writeSnapshotData(snapshotNumber, data) {
+    fs.writeFileSync(
+        `snapshot${snapshotNumber}Out.txt`,
+        Object.entries(data).map(([addr, balance]) => `${addr},${balance}`).join("\n")
+    );
+}
 
 main();
